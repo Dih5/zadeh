0a122d088690f2b3d70bddb7726f4e479d31d9fa
Dih5 <dihedralfive@gmail.com>
2020-12-01 08:17:35 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -3,13 +3,18 @@
 
 """The setup script"""
 
+import os
 from setuptools import setup, find_packages
+
+# Get the long description from the README file
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md')) as f:
+    long_description = f.read()
 
 setup(author="Dih5",
       author_email='dihedralfive@gmail.com',
       classifiers=[
           'Development Status :: 3 - Alpha',
-           # 'Intended Audience :: Science/Research',
+          'Intended Audience :: Science/Research',
           'Natural Language :: English',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.5',
@@ -19,13 +24,16 @@ setup(author="Dih5",
       ],
       description='Python package to build fuzzy inference systems',
       extras_require={
-        "docs": ["nbsphinx", "sphinx-rtd-theme", "IPython"],
-        "test": ["pytest"],
+          "docs": ["nbsphinx", "sphinx-rtd-theme", "IPython"],
+          "test": ["pytest"],
+          "extras": ["matplotlib", "ipywidgets"],
       },
       keywords=[],
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       name='zadeh',
       packages=find_packages(include=['zadeh'], exclude=["demos", "tests", "docs"]),
-      install_requires=[],
+      install_requires=["numpy"],
       url='https://github.com/dih5/zadeh',
       version='0.1.0',
 
