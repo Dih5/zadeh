38077400de575b4be435d58cb347d1b82de7e80a
Dih5 <dihedralfive@gmail.com>
2020-12-11 15:45:57 +0100

    Fix test

--- tests/test_mparser.py
+++ tests/test_mparser.py
@@ -5,6 +5,6 @@ import os
 
 def test_parse():
     """Load an external .fis file"""
-    fis = zadeh.read_mfis(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "tipper.fis"))
+    fis = zadeh.FIS.from_matlab(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "tipper.fis"))
 
     assert isinstance(fis.get_crisp_output({"food": 5, "service": 5}), float)
